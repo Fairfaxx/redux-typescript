54c7da435dfb11c8f14b9e8991d6b87dadddb3a4
FefeLemaire <federico.lemaire@alloy.digital>
2023-01-23 18:32:17 -0300

    problems with npm endpoint, received a 404 error, needs to be check

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
+
 const RepositoriesList: React.FC = () => {
+  const dispatch = useDispatch();
+  const [packageName, setPackageName] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(actionCreators.searchRepositories(packageName) as any);
+  };
+
   return (
     <div>
-      <form>
-        <input />
-        <button>Search</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={packageName}
+          onChange={(e) => setPackageName(e.target.value)}
+        />
+        <button type="submit">Search</button>
       </form>
     </div>
   );
